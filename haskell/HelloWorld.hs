91242268cc19820e785f0d7379f5f78c4a89b726
zadjii <zadjii@gmail.com>
2014-03-11 16:10:38 -0500

    More work learning Haskell.

--- haskell/HelloWorld.hs
+++ haskell/HelloWorld.hs
@@ -1,9 +1,32 @@
 
---Prints Hello World, then prints the even numbers between 1 and 100.
---For The record, I'm not totally certain about the last line, how it works
---exactly. I havent gotten to that part of the tutorial yet.
---But for ~one day of reading I think it's decent.
+--Prints Hello World, then prints the odd & even numbers between 0 and 100.
 main = do
 	putStrLn "Hello World"
-	let evens = map show (takeWhile (<100) (map (*2) [1..]))
-	putStrLn $ show evens
+
+	--This commented section prints the list of odd/even numbers less than 100
+	--	but the printed list is a list of strings of said numbers
+	--let evens = map show (takeWhile (<100) (map (*2) [0..]))
+	--let odds = map show (takeWhile (<100) (map (\x -> 2*x + 1) [0..]))
+	--putStrLn $ show odds
+	--putStrLn $ show evens
+
+	--This version is slightly more readable, and returns a list of Num
+	let odds1 = findNumbersLessThanFittingFunction 100 (\x -> 2*x + 1)
+	let evens1 = findNumbersLessThanFittingFunction 100 (\x -> 2*x)
+	putStrLn $ show odds1
+	putStrLn $ show evens1
+	putStrLn . show . removeDuplicates $ [0..10] ++ [0..10]
+
+
+findNumbersLessThanFittingFunction::(Num a, Ord a, Enum a) => a->(a->a)->[a]
+findNumbersLessThanFittingFunction limit fn = 
+	let functionedElems = map fn [0 ..];
+		underLimit = takeWhile (<limit) functionedElems;
+	in underLimit --underLimit is the return list
+
+--Simple implementation of removing the duplicate elements from a list
+--	could also be done with calling "nub" after including
+--	import Data.List (nub)
+removeDuplicates::(Eq a) => [a] -> [a]
+removeDuplicates xs = 
+	foldr (\x acc -> if (x `elem` acc) then acc else x:acc) [] xs
