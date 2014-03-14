12ca1ba565b54e778971073128476467cc106777
zadjii <zadjii@gmail.com>
2014-03-13 20:52:29 -0500

    Last day of learning Haskell. I did a lot of research on randoms (among other things) in Haskell and decided it wouldn't be too useful to go on learning it. Neat to have in my bag of knowledge, but not worth mastering, similar to Russian.
    I also made a neat little function that creates a decimal number out of a list of ints representing a binary number

--- haskell/HelloWorld.hs
+++ haskell/HelloWorld.hs
@@ -20,10 +20,46 @@ main = do
 	putStrLn $ show . removeDuplicates $ [0..10] ++ [0..10]
 
 	--seed the random number generator to 0, arbitrarily, then get it in rand.
-	setStdGen (mkStdGen 0)
-	rand <- getStdGen
+	--setStdGen (mkStdGen 0)
+	--rand <- getStdGen
+	let rand = mkStdGen 0
 	-- all three of these return the same sequence of numbers, b/c FP :/
-	print $ head $ take 1 (randoms rand :: [Double]) 
+	let randNum = head $ take 1 (randoms rand :: [Double]) 
+	print $ randNum
+	rand <- newStdGen
+	let randNum2 = getRand rand
+	rand <- newStdGen
+	print$ randNum2
+	let randNum2 = getRand rand
+	rand <- newStdGen
+	print$ randNum2
+
+	--Well, it appears there is not really a great way to get reliable random
+	--numbers out of Haskell - although I have demonsrated each iteration of
+	--this code does give you 3 random numbers, it's just a bit too much work
+	--and not straightforward, even in the slightest.
+	--I really love the curried/partial functions idea,
+	--and the general "syntax" is neat, but its overall a bit too difficult to
+	--realize what's actually happening currently.
+	--Probably SUPER awesome for math work, but I'm doing none of that 
+	--currently. 
+	--Like, FP is cool, but as with many modern languages, I feel like I want
+	--more syntax to describe what's happening.
+
+	--But good to have learned this much. Not very useful, but neat to know
+	-- (like Russian :P)
+
+	--let nextRand = 0.0
+	--putStrLn "first rand call"
+	--let (nextRand, newrand) = random rand :: (Double, StdGen)
+	--rand <- fst $ split newrand
+	--putStrLn "after rand call"
+	----putStrLn $ ("Rand 0: " ++ show nextRand)
+	--putStrLn $ show nextRand
+	--let (nextRand2, rand) = random rand :: (Double, StdGen)
+	--print $ "Rand 1: " ++ show nextRand2
+	--rand <- rand'
+	--print $ show nextRand
 	--print $ take 1 (randoms rand :: [Double]) 
 	--print $ take 1 (randoms rand :: [Double]) 
 	-- so yea, I can't generate a totally random number on each and every call
@@ -31,7 +67,15 @@ main = do
 	--rand on each invocation, but not tonight. Tomorrow likely.
 
 
+	print $ binarySum [1, 0, 0, 1]
+	print $ binarySum [1,0,0,1,1,1,0,1,0,1,0,1]
+
+	--print $ finiteRandoms 10 rand 
 	
+
+getRand::StdGen->Double
+getRand rand = head $ take 1 (randoms rand :: [Double]) 
+
 
 
 findNumbersLessThanFittingFunction::(Num a, Ord a, Enum a) => a->(a->a)->[a]
@@ -70,5 +114,13 @@ translateVirtualAddress physicalMem pageSize virtMem ptbr pageTable va =
 			vpn = take (read ( show vpnSize ):: Int) va;
 
 
+binarySum::(Num a, Eq a)=>[a]->a
+binarySum num = foldl (\acc x -> (acc*2)+(if x == 0 then 0 else 1) ) 0 num
 
-
+--From the Learn You A Haskell tutorial (although this I actually found on SO)
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)  
+finiteRandoms 0 gen = ([], gen)  
+finiteRandoms x gen =   
+    let (value, newGen) = random gen  
+        (restOfList, finalGen) = finiteRandoms (x-1) newGen  
+    in  (value:restOfList, finalGen)
